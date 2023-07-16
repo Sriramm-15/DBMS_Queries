4ce10d0e4725e3ece157d3f1ad9c6643b710e997
Sri Ram <118582602+Sriramm-15@users.noreply.github.com>
2023-07-17 05:13:50 +0530

    Create query_3.sql

---
+++ query_3.sql
@@ -0,0 +1,12 @@
+ Find the colors of boats reserved by Lubber
+
+SELECT B.color
+FROM Sailors S, Reserves R, Boats B
+WHERE S.sname = 'Lubber' and S.sid = R.sid and R.bid = B.bid;
+
+/*Output
+color
+red
+green
+red
+*/
