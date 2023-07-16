8e61b85a3f92faf4a87dd7845ddafd53e960c2ab
Sri Ram <118582602+Sriramm-15@users.noreply.github.com>
2023-07-17 05:24:48 +0530

    query_7.sql

---
+++ query_7.sql
@@ -0,0 +1,32 @@
+Q7 Find the names of sailors who have reserved at least two boats.
+
+SELECT S.sname
+FROM Sailors S, Reserves R, Sailors S2, Reserves R2
+WHERE S.sid = R.sid 
+  and S2.sid = R2.sid 
+  and S.sid = S2.sid 
+  and R.bid != R2.bid
+
+/*Output
+SNAME
+Dustin
+Dustin
+Dustin
+Dustin
+Dustin
+Dustin
+Dustin
+Dustin
+Dustin
+Dustin
+Dustin
+Dustin
+Lubber
+Lubber
+Lubber
+Lubber
+Lubber
+Lubber
+Horatio
+Horatio
+*/
