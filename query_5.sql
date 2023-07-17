f945ca322c9f3128f0fa98fc7fe8d17a5625733e
Sri Ram <118582602+Sriramm-15@users.noreply.github.com>
2023-07-17 13:03:37 +0530

    query_5.sql

--- query_5.sql
+++ query_5.sql
@@ -3,16 +3,16 @@
 SELECT S.sname
 FROM Sailors S, Reserves R, Boats B 
 WHERE S.sid = R.sid AND R.bid = B.bid
-      AND (B.color = ‘red’ OR B.color = ‘green’)
+      AND (B.color = 'red' OR B.color = 'green')
 
                   -- or
 SELECT S.sname
 FROM Sailors S, Reserves R, Boats B
-WHERE S.sid = R.sid AND R.bid = B.bid AND B.color = ‘red’
+WHERE S.sid = R.sid AND R.bid = B.bid AND B.color = 'red'
 UNION
 SELECT S2.sname
 FROM Sailors S2, Boats B2, Reserves R2
-WHERE S2.sid = R2.sid AND R2.bid = B2.bid AND B2.color = ‘green’
+WHERE S2.sid = R2.sid AND R2.bid = B2.bid AND B2.color = 'green'
 
 /*Output
 SNAME
