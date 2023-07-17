f1f7accecf8f8d31a4edf08ceae81997e66258a5
Sri Ram <118582602+Sriramm-15@users.noreply.github.com>
2023-07-17 12:58:54 +0530

    query_14.sql

---
+++ query_14.sql
@@ -0,0 +1,16 @@
+--Q14 Find the sids of all sailors who have reserved red boats but not green boats.
+
+using set-difference
+
+SELECT S.sid
+FROM Sailors S, Reserves R, Boats B
+WHERE S.sid = R.sid AND R.bid = B.bid AND B.color = ‘red’
+EXCEPT
+SELECT S2.sid
+FROM Sailors S2, Reserves R2, Boats B2
+WHERE S2.sid = R2.sid AND R2.bid = B2.bid AND B2.color = ‘green’
+
+/*Output
+SID
+64
+*/
