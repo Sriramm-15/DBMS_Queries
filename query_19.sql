81e842fa74c0212b838967cc837612511d6b1243
Sri Ram <118582602+Sriramm-15@users.noreply.github.com>
2023-07-17 13:12:21 +0530

    query_19.sql

--- query_19.sql
+++ query_19.sql
@@ -1,4 +1,4 @@
---Q19 Find the name and age of the oldest sailor.
+--Q19 a) Find the name and age of the oldest sailor.
 
 SELECT S.sname, S.age
 FROM Sailors S
@@ -14,4 +14,22 @@ where S.age >= ALL(SELECT S2.age
 /*Output
 SNAME  AGE
 Bob   63.5
+*/
+
+--Q19 b) Find the names of sailors who are older than the oldest sailor with a rating of 10.
+
+SELECT S.sname
+From Sailors S
+WHERE S.age> (SELECT MAX(S2.age)
+              FROM Sailor S2
+              WHERE S2.rating=10)
+
+/*Output
+SNAME
+Dustin
+Lubber
+Rusty
+Horatio
+Horatio
+Bob
 */
