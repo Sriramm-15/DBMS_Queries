9926f8d3e3ffaecf1a3f2e845239f25c4fe252a7
Sri Ram <118582602+Sriramm-15@users.noreply.github.com>
2023-07-17 13:04:08 +0530

    query_6.sql

--- query_6.sql
+++ query_6.sql
@@ -6,17 +6,17 @@ WHERE S.sid = R1.sid
       AND R1.bid = B1.bid
       AND S.sid = R2.sid 
       AND R2.bid = B2.bid
-      AND B1.color=‘red’ 
-      AND B2.color = ‘green’
+      AND B1.color='red' 
+      AND B2.color = 'green'
       
                -- or 
 SELECT S.sname
 FROM Sailors S, Reserves R, Boats B
-WHERE S.sid = R.sid AND R.bid = B.bid AND B.color = ‘red’
+WHERE S.sid = R.sid AND R.bid = B.bid AND B.color = 'red'
 INTERSECT
 SELECT S2.sname
 FROM Sailors S2, Boats B2, Reserves R2
-WHERE S2.sid = R2.sid AND R2.bid = B2.bid AND B2.color = ‘green’
+WHERE S2.sid = R2.sid AND R2.bid = B2.bid AND B2.color = 'green'
 
 /*Output
 SNAME
