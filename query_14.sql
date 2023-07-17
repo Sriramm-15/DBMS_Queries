07236ff84666c429b0eafda3fc8877cf1889d080
Sri Ram <118582602+Sriramm-15@users.noreply.github.com>
2023-07-17 13:02:25 +0530

    query_14.sql

--- query_14.sql
+++ query_14.sql
@@ -4,11 +4,11 @@ using set-difference
 
 SELECT S.sid
 FROM Sailors S, Reserves R, Boats B
-WHERE S.sid = R.sid AND R.bid = B.bid AND B.color = ‘red’
+WHERE S.sid = R.sid AND R.bid = B.bid AND B.color = 'red'
 EXCEPT
 SELECT S2.sid
 FROM Sailors S2, Reserves R2, Boats B2
-WHERE S2.sid = R2.sid AND R2.bid = B2.bid AND B2.color = ‘green’
+WHERE S2.sid = R2.sid AND R2.bid = B2.bid AND B2.color = 'green'
 
 /*Output
 SID
